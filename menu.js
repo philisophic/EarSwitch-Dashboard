6462f9bf78332e58774eb3e28af03c82f9bd8ddd
rsingh84 <joda844@gmail.com>
2022-06-05 00:54:13 -0700

    Added Light working

--- menu.js
+++ menu.js
@@ -1,3 +1,8 @@
+const HUE_BRIDGE_IP = "";
+const HUE_USERNAME = "";
+
+const axios = require('axios');
+
 (function() {
 	/* Whoever is reading this I am so sorry, I got the code from Codrops but I couldn't figure
 		out how to apply it 6 times efficiently because of event listener callbacks so here's 
@@ -45,4 +50,35 @@
 			closeBttn.addEventListener( 'click', toggleOverlay );
 		})(index);
 	}
+
+
+
+	const turnLightOnOrOff = async (lightId, on) => {
+        const url = `http://${HUE_BRIDGE_IP}/api/${HUE_USERNAME}/lights/${lightId}/state`;
+        try {
+            return await axios.put(url, {
+                on,
+            });
+        } catch (err) {
+            console.error(err);
+        }
+    };
+
+
+
+
+    document.querySelector('.lightOn').addEventListener('click', () => { 
+
+        turnLightOnOrOff(3, true); //turn light 3 on
+
+        
+    }); 
+
+    document.querySelector('.lightOff').addEventListener('click', () => { 
+
+        turnLightOnOrOff(3, false); //turn light 3 on
+
+        
+    }); 
+
 })();
